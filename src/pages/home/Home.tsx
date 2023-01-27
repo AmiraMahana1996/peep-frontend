6e9eaabb60ee0359c6ac62fbc7948518534266ac
AmiraMahana1996 <amiramahana@outlook.com>
2023-01-27 17:06:17 +0200

    init

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Button variant="contained">Text</Button>
+      <h1>Home</h1>
+    </>
+  );
+}
